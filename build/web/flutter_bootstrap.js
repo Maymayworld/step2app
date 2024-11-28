5ec6b623ad1f6ec9d57b1c9f2362fcebbcd64ce4
Maymayworld <4817maymay@gmail.com>
2024-11-29 00:23:14 +0900

    Deploy web app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2615509226"
+    serviceWorkerVersion: "2412770855"
   }
 });
